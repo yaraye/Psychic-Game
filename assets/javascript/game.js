b645b020821af6665fe74d6bf0655a15ae55d39c
Yaraye <yaraye@gmail.com>
2018-05-24 19:11:00 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,23 @@
  "n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 
-var wins=0
-var losses=0
-var guessesLeft=9
+var wins=0;
+var losses=0;
+var guessesLeft=9;
 var yourGuessSoFar="";
 
 //when a key pressed . function will run
 document.onkeypress = function (event) {
+
 //user guessed letter
 var userGuess= event.key;
 console.log (userGuess);
 
-yourGuessSoFar = yourGuessSoFar + "," + userGuess;
-
 //computer guessed result
 var computerChoice =alphabte[Math.floor(Math.random ()*alphabte.length)];
 console.log(computerChoice);
+
+yourGuessSoFar = yourGuessSoFar + " " + userGuess;
 
 //if correct guess say winner and increase win board
 // if wrong increase loss board;
@@ -34,21 +35,23 @@ guessesLeft--;
 alert("try again, you guessed incorrectly");
 }
 
-if (guessesLeft==0){
-losses= losses+1;
+if (guessesLeft===0){
 guessesLeft=9;
 yourGuessSoFar=" ";
+losses = losses+1;
 alert ("you lost, you're not psychic!");
 }
-var guesses = document.getElementById("yourGuessSoFar");
-guesses.innerHTML =  yourGuessSoFar;
 
 var win = document.getElementById("wins");
 wins.innerHTML =  wins;
 
 var loss = document.getElementById("losses");
 losses.innerHTML =  losses;
+
 var guessLeft = document.getElementById("guessesLeft");
 guessLeft.innerHTML = guessesLeft;
+
+var guesses = document.getElementById("yourGuessSoFar");
+guesses.innerHTML =  yourGuessSoFar;
 }
 
